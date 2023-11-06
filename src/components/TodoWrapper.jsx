6a33464f7ffb1f2ab65413b42ad37f20f2027aa1
Vítor Maia <vitor.maia2@senaisp.edu.br>
2023-11-06 08:59:29 -0300

    docs: todoWrapper component documented all functions and classes

--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -4,15 +4,24 @@ import { Todo } from "./Todo";
 import { EditTodoForm } from "./EditTodoForm";
 import { v4 as uuidv4 } from "uuid";
 
+// Recupera a lista de tarefas inicial do LocalStorage ou inicializa um array vazio.
 const initialTodos = JSON.parse(localStorage.getItem("todos")) || [];
 
+/**
+ * Componente `TodoWrapper` é o componente principal que gerencia a lista de tarefas.
+ *
+ * @returns {JSX.Element} - Um elemento JSX que representa a aplicação To-Do.
+ */
 export const TodoWrapper = () => {
+  // Define o estado dos todos, inicializado com a lista recuperada do LocalStorage.
   const [todos, setTodos] = useState(initialTodos);
 
+  // Efeito colateral que atualiza o LocalStorage sempre que a lista de todos é modificada.
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
 
+  // Função para adicionar uma nova tarefa à lista.
   const addTodo = (todoText) => {
     const newTodo = {
       id: uuidv4(),
@@ -23,10 +32,12 @@ export const TodoWrapper = () => {
     setTodos([...todos, newTodo]);
   };
 
+  // Função para excluir uma tarefa da lista.
   const deleteTodo = (id) => {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
+  // Função para alternar o status de conclusão de uma tarefa.
   const toggleComplete = (id) => {
     setTodos(
       todos.map((todo) =>
@@ -35,6 +46,7 @@ export const TodoWrapper = () => {
     );
   };
 
+  // Função para alternar o modo de edição de uma tarefa.
   const editTodo = (id) => {
     setTodos(
       todos.map((todo) =>
@@ -43,6 +55,7 @@ export const TodoWrapper = () => {
     );
   };
 
+  // Função para editar o texto de uma tarefa.
   const editTask = (editedTask, id) => {
     setTodos(
       todos.map((todo) =>
@@ -55,6 +68,7 @@ export const TodoWrapper = () => {
     <div className="TodoWrapper">
       <h1>To Do List</h1>
       <TodoForm addTodo={addTodo} />
+      {/* Mapeia a lista de todos para renderizar cada tarefa ou formulário de edição. */}
       {todos.map((todo) =>
         todo.isEditing ? (
           <EditTodoForm editTodo={editTask} task={todo} key={todo.id} />
